2a8723b355d2de1a17464cf57bdcf49730ce6867
= <=>
2021-06-13 11:55:09 +0800

    change systme name

--- node/src/command.rs
+++ node/src/command.rs
@@ -23,7 +23,7 @@ use node_template_runtime::Block;
 
 impl SubstrateCli for Cli {
 	fn impl_name() -> String {
-		"Substrate Node".into()
+		"Scifanchain".into()
 	}
 
 	fn impl_version() -> String {
